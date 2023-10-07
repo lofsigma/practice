67d673842529de0913dd031f24ae465c1af5ca41
adr <adr@jean-nouvel>
2023-10-07 18:48:15 -0400

    define variables in the loop

--- merge-sorted-array/merge-sorted-array.ts
+++ merge-sorted-array/merge-sorted-array.ts
@@ -4,10 +4,8 @@ export const merge = (
   nums2: number[],
   n: number
 ) => {
-  let j = 0;
-  let k = 0;
   let t = [...nums1];
-  for (let i = 0; i < m + n; i++) {
+  for (let i = 0, j = 0, k = 0; i < m + n; i++) {
     if ((k < m && t[k] > nums2[j]) || k >= m) {
       nums1[i] = nums2[j];
       j++;
