dfbd4b3f7363f0d80d61cd783112c9df5ea80cd0
adr <andres@duarterengifo.com>
2023-10-08 13:16:35 -0400

    speed

--- merge-sorted-array/merge-sorted-array.ts
+++ merge-sorted-array/merge-sorted-array.ts
@@ -1,20 +1,38 @@
+// export const merge = (
+// nums1: number[],
+// m: number,
+// nums2: number[],
+// n: number
+// ) => {
+//   let t = [...nums1];
+//   for (let i = 0, j = 0, k = 0; i < m + n; i++) {
+//     if ((k < m && t[k] > nums2[j]) || k >= m) {
+//       nums1[i] = nums2[j];
+//       j++;
+//       if (k >= m) {
+//         k++;
+//       }
+//     } else {
+//       nums1[i] = t[k];
+//       k++;
+//     }
+//   }
+// };
+
 export const merge = (
   nums1: number[],
   m: number,
   nums2: number[],
   n: number
 ) => {
-  let t = [...nums1];
-  for (let i = 0, j = 0, k = 0; i < m + n; i++) {
-    if ((k < m && t[k] > nums2[j]) || k >= m) {
-      nums1[i] = nums2[j];
-      j++;
-      if (k >= m) {
-        k++;
-      }
+  for (let i = m - 1, j = n - 1, k = m + n - 1; j >= 0; ) {
+    if (i < 0 || nums1[i] < nums2[j]) {
+      nums1[k] = nums2[j];
+      j--;
     } else {
-      nums1[i] = t[k];
-      k++;
+      nums1[k] = nums1[i];
+      i--;
     }
+    k--;
   }
 };
