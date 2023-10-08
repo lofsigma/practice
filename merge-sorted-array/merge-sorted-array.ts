d72bf970010039fe0eaf76b0138f6828fae4280a
adr <andres@duarterengifo.com>
2023-10-08 13:20:44 -0400

    remove comments

--- merge-sorted-array/merge-sorted-array.ts
+++ merge-sorted-array/merge-sorted-array.ts
@@ -1,24 +1,3 @@
-// export const merge = (
-// nums1: number[],
-// m: number,
-// nums2: number[],
-// n: number
-// ) => {
-//   let t = [...nums1];
-//   for (let i = 0, j = 0, k = 0; i < m + n; i++) {
-//     if ((k < m && t[k] > nums2[j]) || k >= m) {
-//       nums1[i] = nums2[j];
-//       j++;
-//       if (k >= m) {
-//         k++;
-//       }
-//     } else {
-//       nums1[i] = t[k];
-//       k++;
-//     }
-//   }
-// };
-
 export const merge = (
   nums1: number[],
   m: number,
